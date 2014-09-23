c0d260abce2605d7a618f8223beeb2ffb2c9ee8e
pshbmw <pshbmw@163.com>
2014-09-23 23:34:18 +0800

    1

--- js/myitems.js
+++ js/myitems.js
@@ -7,27 +7,29 @@ function itemController($scope) {
     $scope.count = '';
     $scope.countAll = '';
     $scope.items = [
-        {id:1, fName:'Apple',  price:4, "image":"img/01.png", "description":"image 01" },
-        {id:2, fName:'Banana',   price:3, "image":"img/02.jpg", "description":"image 02" },
-        {id:3, fName:'Orange',   price:4, "image":"img/03.jpg", "description":"image 03" },
-        {id:4, fName:'Car',  price:12, "image":"img/04.jpg", "description":"image 04" },
-        {id:5, fName:'Lenovo',  price:8, "image":"img/05.jpg", "description":"image 05" },
-        {id:6, fName:'Sony', price:1, "image":"img/06.jpg", "description":"image 06" }
+        {id:1, fName:'Apple',  price:4, image:"img/01.png", description:"image 01",active:false },
+        {id:2, fName:'Banana',   price:3, image:"img/01.png", description:"image 02",active:false },
+        {id:3, fName:'Orange',   price:4, image:"img/01.png", description:"image 03" ,active:false},
+        {id:4, fName:'Car',  price:12, image:"img/01.png", description:"image 04" ,active:false},
+        {id:5, fName:'Lenovo',  price:8, image:"img/01.png", description:"image 05" ,active:false},
+        {id:6, fName:'Sony', price:1, image:"img/01.png", description:"image 06" ,active:false}
     ];
     $scope.edit = true;
     $scope.priceEdit= true;
     $scope.countAllEdit=true;
 
     $scope.edititem = function(id) {
-
         if (id == 'clear') {
             $scope.edit = true;
             $scope.fName = '';
             $scope.price = '';
             $scope.count = '';
             $scope.countAll = 0;
-
         } else {
+            for (var i=0,len=$scope.items.length;i<len;i++) {
+                    $scope.items[i].active=false;
+             }
+            $scope.items[id-1].active=true;
             $scope.edit = false;
             $scope.fName = $scope.items[id-1].fName;
             $scope.price = $scope.items[id-1].price;
